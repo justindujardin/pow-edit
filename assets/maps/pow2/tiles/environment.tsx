2ac076972c9865d22b26822c8b05ac8aff80ab6d
Justin DuJardin <justin@dujardinconsulting.com>
2014-07-13 15:35:56 -0700

    assets: remove eburp and update with updated pow2 maps

--- assets/maps/pow2/tiles/environment.tsx
+++ assets/maps/pow2/tiles/environment.tsx
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Environment" tilewidth="16" tileheight="16">
  <image source="../images/environment.png" width="144" height="128"/>
- <tile id="3">
+ <tile id="2">
   <properties>
    <property name="color" value="#63390e"/>
    <property name="icon" value="bridgeHorizontal.png"/>
@@ -10,7 +10,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="4">
+ <tile id="3">
   <properties>
    <property name="color" value="#925e1a"/>
    <property name="icon" value="caveFloor.png"/>
@@ -19,16 +19,17 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="5">
+ <tile id="4">
   <properties>
    <property name="color" value="#8e6464"/>
    <property name="icon" value="darkBridgeVertical.png"/>
    <property name="opaque" value="false"/>
    <property name="passable" value="true"/>
+   <property name="isPath" value="true"/>
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="6">
+ <tile id="5">
   <properties>
    <property name="color" value="#631c0e"/>
    <property name="icon" value="doorInWall.png"/>
@@ -37,7 +38,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="9">
+ <tile id="8">
   <properties>
    <property name="color" value="#585858"/>
    <property name="icon" value="wall.png"/>
@@ -46,7 +47,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="12">
+ <tile id="11">
   <properties>
    <property name="color" value="#63390e"/>
    <property name="icon" value="bridgeVertical.png"/>
@@ -55,7 +56,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="13">
+ <tile id="12">
   <properties>
    <property name="color" value="#5c3708"/>
    <property name="icon" value="caveWall.png"/>
@@ -64,7 +65,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="14">
+ <tile id="13">
   <properties>
    <property name="color" value="#000000"/>
    <property name="icon" value="darkDoor.png"/>
@@ -73,7 +74,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="17">
+ <tile id="16">
   <properties>
    <property name="color" value="#0808D0"/>
    <property name="icon" value="pool.png"/>
@@ -82,7 +83,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="18">
+ <tile id="17">
   <properties>
    <property name="color" value="#2828f0"/>
    <property name="icon" value="water2.png"/>
@@ -91,16 +92,17 @@
    <property name="shipPassable" value="true"/>
   </properties>
  </tile>
- <tile id="23">
+ <tile id="22">
   <properties>
    <property name="color" value="#8e8e8e"/>
    <property name="icon" value="darkFloor.png"/>
    <property name="opaque" value="false"/>
    <property name="passable" value="true"/>
+   <property name="isPath" value="true"/>
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="24">
+ <tile id="23">
   <properties>
    <property name="color" value="#202020"/>
    <property name="icon" value="dungeonGate.png"/>
@@ -109,7 +111,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="25">
+ <tile id="24">
   <properties>
    <property name="color" value="#909090"/>
    <property name="icon" value="iceMountain.png"/>
@@ -118,7 +120,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="27">
+ <tile id="26">
   <properties>
    <property name="color" value="#a01414"/>
    <property name="icon" value="waterMountain.png"/>
@@ -127,7 +129,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="30">
+ <tile id="29">
   <properties>
    <property name="color" value="#222222"/>
    <property name="icon" value="columnTile.png"/>
@@ -136,7 +138,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="31">
+ <tile id="30">
   <properties>
    <property name="color" value="#8e6464"/>
    <property name="icon" value="darkBridgeHorizontal.png"/>
@@ -145,7 +147,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="32">
+ <tile id="31">
   <properties>
    <property name="color" value="#585858"/>
    <property name="icon" value="darkWall.png"/>
@@ -154,16 +156,17 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="33">
+ <tile id="32">
   <properties>
    <property name="color" value="#8e8e8e"/>
    <property name="icon" value="floor.png"/>
    <property name="opaque" value="false"/>
    <property name="passable" value="true"/>
+   <property name="isPath" value="true"/>
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="34">
+ <tile id="33">
   <properties>
    <property name="color" value="#5b7497"/>
    <property name="icon" value="iceWall.png"/>
@@ -172,7 +175,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="35">
+ <tile id="34">
   <properties>
    <property name="color" value="#06796d"/>
    <property name="icon" value="river.png"/>
@@ -181,7 +184,7 @@
    <property name="shipPassable" value="true"/>
   </properties>
  </tile>
- <tile id="37">
+ <tile id="36">
   <properties>
    <property name="color" value="#544d19"/>
    <property name="icon" value="deadTree.png"/>
@@ -190,7 +193,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="38">
+ <tile id="37">
   <properties>
    <property name="color" value="#925e1a"/>
    <property name="icon" value="deck.png"/>
@@ -199,7 +202,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="39">
+ <tile id="38">
   <properties>
    <property name="color" value="#cd964e"/>
    <property name="icon" value="desert.png"/>
@@ -208,7 +211,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="40">
+ <tile id="39">
   <properties>
    <property name="color" value="#a01414"/>
    <property name="icon" value="desertMountain.png"/>
@@ -217,7 +220,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="43">
+ <tile id="42">
   <properties>
    <property name="color" value="#9eb8be"/>
    <property name="icon" value="iceberg.png"/>
@@ -226,7 +229,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="44">
+ <tile id="43">
   <properties>
    <property name="color" value="#282828"/>
    <property name="icon" value="rubble.png"/>
@@ -235,7 +238,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="46">
+ <tile id="45">
   <properties>
    <property name="color" value="#28f028"/>
    <property name="icon" value="grass.png"/>
@@ -244,7 +247,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="47">
+ <tile id="46">
   <properties>
    <property name="color" value="#5c3708"/>
    <property name="icon" value="gunnelLeft.png"/>
@@ -253,7 +256,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="48">
+ <tile id="47">
   <properties>
    <property name="color" value="#5c3708"/>
    <property name="icon" value="gunnelRight.png"/>
@@ -262,7 +265,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="49">
+ <tile id="48">
   <properties>
    <property name="color" value="#ffffff"/>
    <property name="icon" value="ice.png"/>
@@ -271,16 +274,17 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="50">
+ <tile id="49">
   <properties>
    <property name="color" value="#a8c4dd"/>
    <property name="icon" value="iceFloor.png"/>
    <property name="opaque" value="false"/>
    <property name="passable" value="true"/>
+   <property name="isPath" value="true"/>
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="55">
+ <tile id="54">
   <properties>
    <property name="color" value="#e13e29"/>
    <property name="icon" value="lava.png"/>
@@ -289,7 +293,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="56">
+ <tile id="55">
   <properties>
    <property name="color" value="#631c0e"/>
    <property name="icon" value="lockedDoor.png"/>
@@ -298,7 +302,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="57">
+ <tile id="56">
   <properties>
    <property name="color" value="#a01414"/>
    <property name="icon" value="mountain.png"/>
@@ -307,7 +311,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="61">
+ <tile id="60">
   <properties>
    <property name="color" value="#a0ab40"/>
    <property name="icon" value="palmTree.png"/>
@@ -316,7 +320,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="66">
+ <tile id="65">
   <properties>
    <property name="color" value="#544d19"/>
    <property name="icon" value="stump.png"/>
@@ -325,16 +329,17 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="69">
+ <tile id="68">
   <properties>
    <property name="color" value="#008c00"/>
    <property name="icon" value="treeAlt.png"/>
    <property name="opaque" value="true"/>
    <property name="passable" value="true"/>
+   <property name="isDangerous" value="true"/>
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="70">
+ <tile id="69">
   <properties>
    <property name="color" value="#525f47"/>
    <property name="icon" value="tundra.png"/>
@@ -343,7 +348,7 @@
    <property name="shipPassable" value="false"/>
   </properties>
  </tile>
- <tile id="71">
+ <tile id="70">
   <properties>
    <property name="color" value="#500000"/>
    <property name="icon" value="volcano.png"/>
