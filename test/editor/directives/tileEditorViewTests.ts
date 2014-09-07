e054bddc449e544c20b256a6ddae0486378d4ac2
Justin DuJardin <justin@dujardinconsulting.com>
2014-09-07 14:29:05 -0700

    test(tileEditorView): changing loaded map by modifying scope

--- test/editor/directives/tileEditorViewTests.ts
+++ test/editor/directives/tileEditorViewTests.ts
@@ -2,9 +2,20 @@
 ///<reference path="../../../types/angular/angular-mocks.d.ts"/>
 ///<reference path="../../../assets/build/pow-edit.d.ts"/>
 
+
+/**
+ * TODO: Update Scaffolding Test
+ *
+ * This test is only set up to very crudely wire together an async check
+ * for loading an existing TMX map.  It should be updated, and the tile
+ * editor class should be refactored as tests are added.
+ *
+ */
+
 describe("pow2.editor",()=>{
    var element:ng.IAugmentedJQuery;
    var scope:any;
+   var editor:pow2.editor.TileEditorController = null;
    beforeEach(module('pow-edit'));
    beforeEach(module('source/editor/directives/tileEditorView.html'));
    beforeEach(inject(($rootScope,$compile:ng.ICompileService) => {
@@ -15,6 +26,7 @@ describe("pow2.editor",()=>{
       $compile(element)(scope);
       $('body').append(element);
       scope.$digest();
+      editor = <pow2.editor.TileEditorController>element.controller('tileEditorView');
    }));
    afterEach(() => {
       element.remove();
@@ -25,6 +37,19 @@ describe("pow2.editor",()=>{
    it("should notify scope of load event",(done)=>{
       scope.$on('map-loaded',()=>{
          done();
+      });
+      scope.$digest();
+   });
+   it("should bind map to url attribute changes",(done)=>{
+      var off = scope.$on('map-loaded',()=>{
+         expect(editor.tileMap.name).toBe(scope.mapUrl);
+         off();
+         off = scope.$on('map-loaded',()=>{
+            expect(editor.tileMap.name).toBe(scope.mapUrl);
+            done();
+         });
+         scope.mapUrl = 'base/test/fixtures/example2.tmx';
+         scope.$digest();
       });
       scope.$digest();
    });
